12c88e653d8a61c3a7f1410b4f9a77de5b81bed5
Paola Machado <paolamds06@gmail.com>
2021-12-23 21:05:56 -0300

    feat: github auth and payment

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,18 +1,28 @@
+import styles from "./styles.module.scss";
 import { FaGithub } from "react-icons/fa";
 import { FiX } from "react-icons/fi";
-import styles from "./styles.module.scss";
+import { signIn, signOut, useSession } from "next-auth/client";
 
 export function SignInButton() {
-  const isUserLoggedIn = true;
+  const [session] = useSession();
+  console.log(session);
 
-  return isUserLoggedIn ? (
+  return session ? (
     <button type="button" className={styles.signInButton}>
       <FaGithub color="#04d361" />
-      Paola Machado
-      <FiX color="#737380" className={styles.closeIcon} />
+      {session.user.name}
+      <FiX
+        color="#737380"
+        className={styles.closeIcon}
+        onClick={() => signOut()}
+      />
     </button>
   ) : (
-    <button type="button" className={styles.signInButton}>
+    <button
+      type="button"
+      className={styles.signInButton}
+      onClick={() => signIn("github")}
+    >
       <FaGithub color="#eba417" />
       Sign in with Github
     </button>
